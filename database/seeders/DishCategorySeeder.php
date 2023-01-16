0ceba2dd4af76a9ed512e47eebe6d4872ebfafdc
AndreGallini <andrea.gallini01@libero.it>
2023-01-16 17:10:25 +0100

    review/dishcategory seeder/model

--- database/seeders/DishCategorySeeder.php
+++ database/seeders/DishCategorySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\DishCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
 
 class DishCategorySeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class DishCategorySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newDishCategory = new DishCategory;
+            $newDishCategory->name = $faker->sentence(5);
+            $newDishCategory->save();
+
+
+        }
     }
 }
--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Review;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
 
 class ReviewSeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class ReviewSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++){
+            $newreview = new Review;
+            $newreview->title = $faker->sentence(5);
+            $newreview->text = $faker->sentence(30);
+            $newreview->vote = $faker->numberBetween(0, 5);
+            $newreview->save();
+        }
     }
 }
