15529cf85cf9f87e04a8fe5c18b708bf1cbf911f
mattialicausi <licausivitomattia@gmail.com>
2023-01-16 17:09:17 +0100

    added dishee seeder

--- database/seeders/DisheSeeder.php
+++ database/seeders/DisheSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Dishe;
+
+use Faker\Generator as Faker;
+
 
 class DisheSeeder extends Seeder
 {
@@ -12,8 +16,16 @@ class DisheSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++) {
+            $dish = new Dishe();
+
+            $dish->name = $faker->sentence(2);
+            $dish->img = $faker->image(null, 640, 480);
+            $dish->price = $faker->numberBetween(0, 100);
+            $dish->description = $faker->sentence(10);
+            $dish->save();
+        }
     }
 }
