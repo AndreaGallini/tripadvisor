48673559ee34edbdd54534e69abcd854842b332b
AndreGallini <andrea.gallini01@libero.it>
2023-01-16 17:44:33 +0100

    firstr relation activity reviews

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Activity extends Model
@@ -13,5 +15,10 @@ class Activity extends Model
     protected $guarded = [];
     public static function generateSlug($name) {
         return Str::slug($name, '-');
+    }
+
+        public function review() :HasMany
+    {
+        return $this->hasMany(Review::class);
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Review extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function activity():BelongsTo
+    {
+        return $this->belongsTo(Activity::class);
+    }
 }
