0c72d8727e4dab9e25cb98ee511a14e1e0157c8c
GioGreco <grecogiorgio.97@gmail.com>
2023-01-16 17:07:31 +0100

    category+activity seeder/model

--- database/seeders/ActivitySeeder.php
+++ database/seeders/ActivitySeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\Activity;
 
 class ActivitySeeder extends Seeder
 {
@@ -12,8 +15,19 @@ class ActivitySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 5; $i++){
+            $activity = new Activity();
+            $activity->name = $faker->sentence(2);
+            $activity->address = $faker->address();
+            $activity->img_cover = $faker->imageUrl(640, 480);
+            $activity->slug = Str::slug($activity->name, '-');
+            $activity->img = $faker->imageUrl(300, 300, 'animals');
+            $activity->phone_number = $faker->ean13();
+            $activity->email = $faker->email();
+            $activity->website = $faker->freeEmail();
+            $activity->save();
+        }
     }
 }
